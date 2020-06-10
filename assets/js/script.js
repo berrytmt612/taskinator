44eda61146598412bb7967b98e471e7320a45341
unknown <berrytmt612@gmail.com>
2020-06-09 21:17:19 -0500

    end of 4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,21 @@ var createTaskHandler = function() {
     
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add html content to div 
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>"; 
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl); 
 }
 
 formEl.addEventListener("submit", createTaskHandler); 
